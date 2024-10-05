3be125d3d363c2d2be7e7c86b8bb32d9905dac39
Liubov Kovalova <liuba532013@gmail.com>
2024-10-05 14:02:23 -0300

    Added header and images to the App Component

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
+import image from "./todo.jpg";
+import imageDone from "./done.jpg";
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <img src={ image } width="400px" alt="To Do List" />
+
+      <h1>To Do List</h1>
+
+      <img src={ imageDone } width="400px" alt="Done" />
     </div>
   );
 }
