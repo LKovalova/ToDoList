875d486d3d591d4936d6c7548b1044e1cc54c397
Liubov Kovalova <liuba532013@gmail.com>
2024-10-06 12:51:48 -0300

    Added button Add, and displayed list of tasks

--- todo-list/src/ToDoList.js
+++ todo-list/src/ToDoList.js
@@ -1,26 +1,43 @@
-import { Component } from 'react';
+import { Component } from "react";
 
 export class ToDoList extends Component {
-    state = {
-        input: "",
-        list: []
-    }
+  state = {
+    input: "",
+    list: [],
+  };
 
-    onChangeEvent(event) {
-        this.setState({input: event})
-        console.log(event);
-        
-    }
+  onChangeEvent(event) {
+    this.setState({ input: event });
+  }
 
-    render() {
-        return(
-            <div>
-                <input 
-                type="text" 
-                placeholder='What do you want to do?'
-                onChange= {(event) => { this.onChangeEvent(event.target.value)}}
-                value= { this.state.input }/>
-            </div>
-        )
-    }
+  addItem(input) {
+    let listArray = this.state.list;
+    listArray.push(input);
+    this.setState({ list: listArray, input: "" });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <input
+            type="text"
+            placeholder="What do you want to do?"
+            onChange={(event) => {
+              this.onChangeEvent(event.target.value);
+            }}
+            value={this.state.input}
+          />
+        </div>
+        <div>
+          <button onClick={() => this.addItem(this.state.input)}>Add</button>
+        </div>
+        <ul>
+          {this.state.list.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
